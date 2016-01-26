2f7fd410a0b7441f7b19027b448c995b44165ad4
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-26 21:34:03 +0900

    refs #3 登録（更新）用のメソッドを実装する

--- memcache551.go
+++ memcache551.go
@@ -1,7 +1,14 @@
 package memcache551
 
+import (
+	"encoding/json"
+	"github.com/bradfitz/gomemcache/memcache"
+)
+
 type Memcache struct {
 	config *Config
+	client *memcache.Client
+	sid    string
 }
 
 type Config struct {
@@ -10,8 +17,29 @@ type Config struct {
 	Expires int32  `json:"expires"`
 }
 
-func New(config *Config) *Memcache {
-	return &Memcache{
+func New(config *Config, sid string) *Memcache {
+	m := &Memcache{
 		config: config,
+		sid:    sid,
+		client: memcache.New(config.Host),
 	}
+
+	return m
+}
+
+func (m *Memcache) Set(name string, value interface{}) {
+	val, err := json.Marshal(value)
+	if err != nil {
+		panic(err)
+	}
+
+	err = m.client.Set(&memcache.Item{
+		Key:        m.config.Prefix + ":" + m.sid + ":" + name,
+		Value:      val,
+		Expiration: m.config.Expires,
+	})
+	if err != nil {
+		panic(err)
+	}
+
 }
--- memcache551_test.go
+++ memcache551_test.go
@@ -2,6 +2,7 @@ package memcache551_test
 
 import (
 	"github.com/go51/memcache551"
+	"github.com/go51/secure551"
 	"testing"
 )
 
@@ -11,9 +12,10 @@ func TestLoad(t *testing.T) {
 		Prefix:  "gorai",
 		Expires: 3600,
 	}
+	sid := secure551.Hash()
 
-	m1 := memcache551.New(&config)
-	m2 := memcache551.New(&config)
+	m1 := memcache551.New(&config, sid)
+	m2 := memcache551.New(&config, sid)
 
 	if m1 == nil {
 		t.Errorf("インスタンス生成に失敗しました。")
@@ -32,8 +34,38 @@ func BenchmarkLoad(b *testing.B) {
 		Prefix:  "gorai",
 		Expires: 3600,
 	}
+	sid := secure551.Hash()
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = memcache551.New(&config)
+		_ = memcache551.New(&config, sid)
 	}
+}
+
+func TestSet(t *testing.T) {
+	config := memcache551.Config{
+		Host:    "localhost:11211",
+		Prefix:  "gorai",
+		Expires: 3600,
+	}
+	sid := secure551.Hash()
+
+	m := memcache551.New(&config, sid)
+	m.Set("test", "test_string")
+}
+
+func BenchmarkSet(b *testing.B) {
+	config := memcache551.Config{
+		Host:    "localhost:11211",
+		Prefix:  "gorai",
+		Expires: 3600,
+	}
+	sid := secure551.Hash()
+	m := memcache551.New(&config, sid)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Set("test", "test_string")
+	}
+
 }
